6a7698c007cea0016ca422a47aa74bfdce042576
Arka Banerjee <arkabanerjee381@gmail.com>
2026-07-13 11:17:55 +0530

    java: printing learning

--- output.java
+++ output.java
@@ -2,7 +2,8 @@ public class output {
     public static void main(String args[]){
 
         //output 
-        
-        System.out.println("output is this");
+
+        System.out.println("output is this"); // this is the line and gets done with the cursor in the next line
+        System.out.print("this output is also there"); // simple print
     }
 }
