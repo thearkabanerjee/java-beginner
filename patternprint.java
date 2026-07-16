2e2468b5f9ae353415035a71b79f0dcf0bf348f6
Arka Banerjee <arkabanerjee381@gmail.com>
2026-07-16 17:30:02 +0530

    java: patternprint

---
+++ patternprint.java
@@ -0,0 +1,5 @@
+public class patternprint{
+    public static void main (String args[]){
+        System.out.println("*\n**\n***\n****"); // print ~ we can use but that wont end the statement
+    }
+}
