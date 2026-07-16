904c63f65abfc2552cd963206f5b3c86edf41998
Arka Banerjee <arkabanerjee381@gmail.com>
2026-07-16 18:07:08 +0530

    java: add subs

---
+++ addsubs.java
@@ -0,0 +1,12 @@
+public class addsubs {
+    public static void main(){
+        int a = 12;
+        int b = 13;
+
+        int sum = a+b;
+        int diff = b-a;
+
+        System.out.println ("differnce is " + diff);
+        System.out.println ("sum is " + sum);
+    }
+}
