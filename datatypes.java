165ef8d29f6cdbc3b25bcf738431e061ad7a074c
Arka Banerjee <arkabanerjee381@gmail.com>
2026-07-16 17:56:12 +0530

    java: datatypes

---
+++ datatypes.java
@@ -0,0 +1,10 @@
+public class datatypes{
+    public static void main(String[] args){
+        String name = "Arka banerjee";
+        int age = 20;
+        double price = 12.24;
+
+
+        System.out.println (name+ "," + age + "," + price);
+    }
+}
