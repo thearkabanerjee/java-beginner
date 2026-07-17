8b4f0cb10e1059886f721ad53725ccc14523d322
Arka Banerjee <arkabanerjee381@gmail.com>
2026-07-17 17:02:48 +0530

    java: question 1

---
+++ question1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class question1 {
+    public static void main(){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int age = a+b;
+
+        System.out.println ("the sum is "+ age);
+
+
+    }
+}
