ef78761ba29284155ad7036f4ca9456221915fe1
Arka Banerjee <arkabanerjee381@gmail.com>
2026-07-17 17:46:41 +0530

    type casting

---
+++ typecasting.java
@@ -0,0 +1,12 @@
+public class typecasting {
+    public static void main(String[] args){
+
+        int a = 22;
+
+        double b = (double)a;
+        String c = "arka";
+
+        System.out.println(b + " is a float");
+        System.out.println(c.getClass());
+    }
+}
