decf26d6cc725db73ac7361ccc98a24807ce32c2
Arka Banerjee <arkabanerjee381@gmail.com>
2026-07-17 16:59:06 +0530

    java : import statement

---
+++ importi.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class importi{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        
+        int age = sc.nextInt();
+
+
+        System.out.println(name);
+        System.out.println (age);
+        sc.close();
+    }
+}
