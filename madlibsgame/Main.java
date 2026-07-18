ec92561aeb5ce2b9ae0de9ca9527cac5911dca37
Arka Banerjee <arkabanerjee381@gmail.com>
2026-07-18 13:45:42 +0530

    java: mad libs game

---
+++ madlibsgame/Main.java
@@ -0,0 +1,12 @@
+package madlibsgame;
+
+import java.util.*;
+
+public class Main {
+   public static void main (String[] args){
+    Scanner sc = new Scanner(System.in);
+    String firstword = sc.nextLine();
+    System.out.println (firstword);
+    sc.close();
+   } 
+}
